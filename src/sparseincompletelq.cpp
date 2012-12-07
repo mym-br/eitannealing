e87ce736854b9ccf4e4dfc9ba7f85f4a29139b3c
Thiago Martins <thiago@usp.br>
2012-12-07 20:03:44 -0200

    decomposition pseudocode

--- src/sparseincompletelq.cpp
+++ src/sparseincompletelq.cpp
@@ -1,2 +1,17 @@
 #include "sparseincompletelq.h"
 
+
+
+SparseIncompleteLQ::SparseIncompleteLQ(
+  const Eigen::SparseMatrix<double, Eigen::SelfAdjoint | Eigen::LowerTriangular | Eigen::ColMajor >& matrix, unsigned int iq, unsigned int il)
+{
+    // Q Matrix (As a vector of sparse vectors)
+    //
+    // building Q vector
+    
+    // Q rows
+    // building L vector
+    // should we SORT L vector?
+    
+    
+}

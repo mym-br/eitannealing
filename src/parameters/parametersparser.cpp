e7472282a27f2f789bae95faa65a8e30c102b2b6
Andre Sato <andresato@hotmail.com>
2018-05-16 10:12:37 -0300

    Changed temperature and regularization input parameters as optional.

--- src/parameters/parametersparser.cpp
+++ src/parameters/parametersparser.cpp
@@ -20,6 +20,9 @@ CommandLineParseResult parseCommandLine(QCommandLineParser &parser, EitAnnealing
 	const QCommandLineOption gndOpt("ground", "Ground node.", "number"); parser.addOption(gndOpt);
 	const QCommandLineOption currentsOutOpt("currentsout", "Input removed currents file.", "number"); parser.addOption(currentsOutOpt);
 	const QCommandLineOption calibrationOpt("calibration", "Calibration mode: 1 - single coefficient for electrodes; 2 - individual coefficients for electrodes.", "number"); parser.addOption(calibrationOpt);
+	const QCommandLineOption ktOpt("kt", "Initial temperature.", "number"); parser.addOption(ktOpt);
+	const QCommandLineOption regularizationOpt("regularization", "Weigth for the gradirnt norm regularization.", "number"); parser.addOption(regularizationOpt);
+	const QCommandLineOption electrodeVarOpt("elecvar", "Weigth for the electrode variance regularization.", "number"); parser.addOption(electrodeVarOpt);
 
     const QCommandLineOption helpOption = parser.addHelpOption();
     const QCommandLineOption versionOption = parser.addVersionOption();
@@ -35,25 +38,22 @@ CommandLineParseResult parseCommandLine(QCommandLineParser &parser, EitAnnealing
     if (parser.isSet(helpOption))
         return CommandLineHelpRequested;
 
-    const QStringList positionalArguments = parser.positionalArguments();
-    if (positionalArguments.isEmpty()) {
-        *errorMessage = "No argument specified.";
-        return CommandLineError;
-    }
-	if (positionalArguments.size() < 6) {
+	const QStringList positionalArguments = parser.positionalArguments();
+	if (positionalArguments.isEmpty()) {
+		*errorMessage = "No argument specified.";
+		return CommandLineError;
+	}
+	if (positionalArguments.size() < 3) {
 		*errorMessage = "Too few arguments.";
 		return CommandLineError;
 	}
-    if (positionalArguments.size() > 6) {
-        *errorMessage = "Too many arguments specified.";
-        return CommandLineError;
-    }
+	if (positionalArguments.size() > 3) {
+		*errorMessage = "Too many arguments specified.";
+		return CommandLineError;
+	}
     params->inputMesh = positionalArguments.at(0);
 	params->inputCurrents = positionalArguments.at(1);
 	params->inputTensions = positionalArguments.at(2);
-	params->regularizationFactor = positionalArguments.at(3).toDouble();
-	params->electrodevar = positionalArguments.at(4).toDouble();
-	params->kt = positionalArguments.at(5).toDouble();
 	if (parser.isSet(currentsOutOpt)) params->inputCurrentsOut = parser.value(currentsOutOpt);
 	if (parser.isSet(gmeshOpt)) params->gmeshAddress = parser.value(gmeshOpt); else params->gmeshAddress = "127.0.0.1:44202";
 	if (parser.isSet(outputOpt)) params->outputMesh = parser.value(outputOpt); else params->outputMesh = "solution.msh";
@@ -61,6 +61,9 @@ CommandLineParseResult parseCommandLine(QCommandLineParser &parser, EitAnnealing
 	if (parser.isSet(peparamOpt)) params->peParam = parser.value(peparamOpt).toFloat(); else params->peParam = 0.875f;
 	if (parser.isSet(gndOpt)) params->ground = parser.value(gndOpt).toInt(); else params->ground = -1;
 	if (parser.isSet(calibrationOpt)) params->calibrationMode = parser.value(calibrationOpt).toInt();
+	if (parser.isSet(ktOpt)) params->kt = parser.value(ktOpt).toDouble(); else params->kt = 0.05;
+	if (parser.isSet(regularizationOpt)) params->regularizationFactor = parser.value(regularizationOpt).toDouble(); else params->regularizationFactor = 30;
+	if (parser.isSet(electrodeVarOpt)) params->electrodevar = parser.value(electrodeVarOpt).toDouble(); else params->electrodevar = 0;
 
     return CommandLineOk;
 }

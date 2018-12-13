85843fb2d7878c1ed5528592579254478aaedfbc
Thiago Martins <thiago@usp.br>
2018-12-13 10:09:04 -0200

    duplicated main.cpp to mainlb.cpp

--- src/maintwosteps.cpp
+++ src/maintwosteps.cpp
@@ -182,7 +182,7 @@ void workProc()
 	iterations = 0;
 	int no_avance_count = 0;
 	double prevE = 10000000000.0;
-	while (kt > 0.00000000005 && no_avance_count < 3) {
+	while (kt > 0.01 && no_avance_count < 3) {
 		e = sqe = r = 0; v = 0;
 		totalit = acceptit = 0;
 		solutions = 0;
@@ -242,23 +242,8 @@ void workProc()
 		//probe.saturate();
 		int nObs = isComplexProblem ? readingsComplex->getNObs() : readingsScalar->getNObs();
 		std::cout << kt << ":" << totalit << ":" << eav << ":" << sige << ":" << rav << ":" << vav << ":" << ((float)iterations) / (nObs*solutions) << ":" << seed << std::endl;
-		//std::cout << "last:" << current->getDEstimate() << " real:" << probe.getDEstimate() <<  std::endl;
-		/*for(int it=0;it<numcoefficients;it++) {
-		std::cout << it << ":" << current->getSolution()[it] << std::endl;
-		}*/
-
-		/*solution_lb probe(current->getSolution());
-		probe.saturate();
-		std::cout << "last (max):" << current->getDMax() << "last (min):" << current->getDMin() << " LB:" << probe.getDEstimate() <<  std::endl;
-		*//*for(int kk=0;kk<9000;kk++) {
-		std::cout << (kk/32) << " Dest:" << current->getDEstimate() << std::endl;
-		current->improve();
-		}*/
-
-
-
-
 		kt *= 0.9f;
+
 		double variation = isComplexProblem ? fabs(prevE - currentComplex->getDEstimate()) / prevE : fabs(prevE - currentScalar->getDEstimate()) / prevE;
 		//std::cout << "totalit:" << iterations << std::endl;
 		//std::cout << "variation: " << variation << std::endl;
@@ -273,104 +258,7 @@ void workProc()
 	//probe.saturate();
 	//std::cout << "real:" << probe.getDEstimate() << " iterations: " << iterations << std::endl;
 
-#ifdef GGGGGGGGGGGG
 
-	boost::mt11213b rng(std::clock());
-
-	// Prepare a current vector, flowing from left to right
-	Eigen::VectorXd current(numNodes - 1);
-	int i;
-	Eigen::VectorXd tension(numNodes - 1);
-	for (i = 0; i<numNodes - 1; i++) tension[i] = i % 10 - 5;
-	current = *stiffness * tension;
-	/*// Left electrodes (but the top left)
-	for(i=0;i<7;i++) current[i] = -1;
-	// Bottom
-	for(;i<7+8;i++) current[i] = 0;
-	// Right
-	for(;i<7+8+8;i++) current[i] = 1;
-	// Top and the rest
-	for(;i<numNodes-1;i++) current[i] = 0;*/
-
-	// Now solve for tensions
-
-
-	Eigen::VectorXd error1, error2;
-	Eigen::VectorXd error;
-	//Eigen::VectorXd perror;
-	SparseIncompleteLLT precond(*stiffness);
-	CG_Solver solver(*stiffness, current, precond);
-	//CG_PrecondSolver psolver(*stiffness, current);
-	/*
-	matrix jacobi = solver.buildJacobiMatrx();
-	Eigen::QR<Eigen::MatrixXd> jacobi_qr(jacobi.toDense());
-	Eigen::VectorXd e1(jacobi.rows());
-	e1.fill(0); e1[0] = 1.0;
-	Eigen::VectorXd w(jacobi_qr.matrixR().transpose().solveTriangular(e1));
-	std::cout << jacobi << std::endl;
-	std::cout << "W:\n";
-	std::cout << w << std::endl;
-	std::cout << "norm:" << w.squaredNorm();
-	std::cout << "Using jacobi:    " << current.squaredNorm()*w.squaredNorm() << std::endl;*/
-	Eigen::VectorXd preTension(tension);
-	solver.getPrecond().halfMultInPlace(preTension);
-	std::cout << "Using stiffness: " << preTension.squaredNorm() << std::endl;
-	//std::cout << "Using stiffness: " << sqrt(tension.dot(preTension)) << std::endl;
-	std::cout << "Using stiffness (l2): " << tension.squaredNorm() << std::endl;
-
-
-
-	for (i = 0; i<numNodes; i++) {
-		error = preTension - solver.getY();
-		//perror = tension - psolver.getX();
-
-		solver.do_iteration();
-		//psolver.do_iteration();
-		//if((solver.getIteration() % 30)==0)
-		//solver.setrefresh();
-
-
-		std::cout << solver.getIteration() << ":" << sqrt(error.squaredNorm()) << ":" << sqrt(solver.getErrorl2Estimate()) << std::endl;
-	}
-	//matrix jacobi = solver.buildJacobiMatrx();
-
-
-
-
-	/*
-	CG_Solver solver0(*stiffness0, current, tension);
-
-	for(i=0;i<numNodes+30;i++) {
-	solver0.do_iteration();
-	}
-
-	CG_Solver solver1(*stiffness0, current, tension);
-	CG_PrecondSolver psolver1(*stiffness0, current, tension);
-	Eigen::VectorXd oldtension(tension);
-	tension = solver0.getX();
-
-	for(i=0;i<numNodes+30;i++) {
-	error1 = tension - solver1.getX();
-	error2 = tension - psolver1.getX();
-
-	solver1.do_iteration();
-	psolver1.do_iteration();
-
-	if((psolver1.getIteration() % 30)==0)
-	psolver1.setrefresh();
-
-	std::cout << solver1.getIteration() << ":" << error1.lpNorm<2>() << ":" << solver1.getErrorl2Estimate() << std::endl;
-	//std::cout << solver.getIteration() << ":" << norm.rows() << "," << norm.cols() << std::endl;
-	//std::cout << solver.getIteration() << ":" << solver.getResidueSquaredNorm() << std::endl;
-	}
-
-	double totalerror = 0;
-	for(i=0;i<31;i++) {
-	totalerror += (solver1.getX()[i] - oldtension[i])*(solver1.getX()[i] - oldtension[i]);
-	}
-	totalerror = sqrt(totalerror);
-	std::cout << "Total error:" << totalerror << std::endl;*/
-#endif //#ifdef GGGGGGGGGGGG
 	QApplication::quit();
 }
 
@@ -411,7 +299,7 @@ int main(int argc, char *argv[])
 	QApplication app(argc, argv);
 	QApplication::setApplicationName("EIT Annealing Test");
 	QApplication::setApplicationVersion("0.1");
-	 
+
 
 	// --> Parse command line arguments
 	QCommandLineParser parser;

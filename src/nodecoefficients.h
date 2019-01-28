7670d60c287b2559fb8f00972162a68322d941c4
Thiago Martins <thiago@usp.br>
2019-01-28 13:05:06 -0200

    replaced NULL -> nullptr

--- src/nodecoefficients.h
+++ src/nodecoefficients.h
@@ -19,7 +19,7 @@ struct nodeCoefficients {
 	nodeCoefficients *next;
 
 	nodeCoefficients(int node, int index, double coefficient):
-		node(node), condIndex(index), coefficient(coefficient), next(NULL) {}
+		node(node), condIndex(index), coefficient(coefficient), next(nullptr) {}
 
 	nodeCoefficients(int node, int index, double coefficient, nodeCoefficients *next):
 			node(node), condIndex(index), coefficient(coefficient), next(next) {}

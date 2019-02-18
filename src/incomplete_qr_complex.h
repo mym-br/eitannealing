3f33cdf9b8e7bbd5a20ea8e19dd6795f01b20f5c
Thiago Martins <thiago@usp.br>
2019-02-18 20:53:35 -0300

    incomplete qr preconditioner: joined imaginary and real parts on cols and rows, wrote solveInPlace methods.

--- src/incomplete_qr_complex.h
+++ src/incomplete_qr_complex.h
@@ -20,10 +20,8 @@ template<class scalar> class SparseIncompleteQRComplex
 {
   protected:
     std::vector<double> idiagonal;
-    std::vector<std::unordered_map<unsigned long, double> > rowsR;
-    std::vector<std::unordered_map<unsigned long, double> > rowsI;
-    std::vector<std::unordered_map<unsigned long, double> > colsR;
-    std::vector<std::unordered_map<unsigned long, double> > colsI;
+    std::vector<std::unordered_map<unsigned long, std::complex<double> > > rows;
+    std::vector<std::unordered_map<unsigned long, std::complex<double> > > cols;
 
     struct MatricesStorageAdaptor {
         const Eigen::SparseMatrix<double, Eigen::ColMajor> &iiR;
@@ -66,24 +64,42 @@ template<class scalar> class SparseIncompleteQRComplex
     SparseIncompleteQRComplex(
       const matrix &iiUpper_R, const matrix &iiUpper_I,
       const matrix &Aic_R, const matrix &Aic_I
-    ): idiagonal(iiUpper_R.rows()), rowsR(iiUpper_R.rows()),
-      rowsI(iiUpper_R.rows()), colsR(iiUpper_R.rows()), colsI(iiUpper_R.rows()) {
-      idiagonal.reserve(iiUpper_R.size());
+    ): idiagonal(iiUpper_R.rows()), rows(iiUpper_R.rows()), cols(iiUpper_R.rows()) {
+
       SparseIncompleteQRBuilder<std::complex<double> > builder;
 
-
       builder.buildRMatrixFromColStorage(MatricesStorageAdaptor(iiUpper_R, iiUpper_I, Aic_R, Aic_I), 8, 16, [this](unsigned long j, double x) {
-        this->idiagonal[j] = x;
+        this->idiagonal[j] = 1/x;
       }, [this](unsigned long i, unsigned long j, std::complex<double> x) {
-        this->rowsR[i][j] = x.real();
-        this->rowsI[i][j] = x.imag();
-        this->colsR[j][i] = x.real();
-        this->colsI[j][i] = x.imag();
+        this->rows[i][j] = x;
+        this->cols[j][i] = x;
       });
     }
 
-    bool solveInPlaceC(Eigen::VectorXd &bR, Eigen::VectorXd &bI) const;
-    bool solveInPlaceCT(Eigen::VectorXd &bR, Eigen::VectorXd &bI) const;
+    void solveInPlaceC(Eigen::VectorXd &bR, Eigen::VectorXd &bI) const {
+      unsigned long i = idiagonal.size();
+      for(; i>=0; i--) {
+        for(auto [j, x] : rows[i]) {
+          bR[i] -= bR[j]*x.real() - bI[j]*x.imag();
+          bI[i] -= bR[j]*x.imag() + bI[j]*x.real();
+        }
+        bR[i] *= idiagonal[i];
+        bI[i] *= idiagonal[i];
+      }
+    };
+
+     // conjugated transpose
+    void solveInPlaceCT(Eigen::VectorXd &bR, Eigen::VectorXd &bI) const {
+      unsigned long n = idiagonal.size();
+      for(unsigned int i = 0; i<n; i++) {
+        for(auto [j, x] : cols[i]) {
+          bR[i] -= bR[j]*x.real() + bI[j]*x.imag();
+          bI[i] -= bI[j]*x.real() - bR[j]*x.imag();
+        }
+        bR[i] *= idiagonal[i];
+        bI[i] *= idiagonal[i];
+      }
+    };
 };
 
 

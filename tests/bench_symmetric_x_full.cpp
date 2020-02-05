64e95fe676246290154199468e6728538998cd75
Thiago Martins <thiago@usp.br>
2020-02-05 14:14:55 -0300

    Added transposed benchmarks

--- tests/bench_symmetric_x_full.cpp
+++ tests/bench_symmetric_x_full.cpp
@@ -27,6 +27,7 @@ int main(int argc, char *argv[])
     	 std::cerr << "need mesh filename\n";
     	 return 1;
      }
+     //Eigen::setNbThreads(16);
      std::cout << "Parsing mesh file..." << std::flush;
      std::shared_ptr<problem> input(problem::createNewProblem(argv[1], &is2d));
      input->initProblem(argv[1]);
@@ -48,28 +49,65 @@ int main(int argc, char *argv[])
      assembleProblemMatrix_lb(sol_I, &Aii_I, &Aic_I, &Acc_I, *input);
      std::cout << "Done\n" << std::flush;
 
-     matrix c(Aii_R->selfadjointView<Eigen::Lower>());
-     Eigen::VectorXd  x(Eigen::VectorXd::Zero(c.rows())), y;
+     matrix ii(Aii_R->selfadjointView<Eigen::Lower>());
+     matrix c(Aii_R->rows()+Aic_R->rows(), Aii_R->cols());
+     
+     c.reserve(ii.nonZeros() + Aic_R->nonZeros());
+     for(int i = 0; i < Aii_R->cols(); i++) {
+        c.startVec(i);
+        for(Eigen::SparseMatrix<double>::InnerIterator iti(ii, i); iti; ++iti) {
+            c.insertBack(iti.row(), i) = iti.value();
+        }
+        for(Eigen::SparseMatrix<double>::InnerIterator itc(*Aic_R, i); itc; ++itc) {
+            c.insertBack(itc.row(), i) = itc.value();
+        }     
+     }
+     c.finalize();
+     
+     Eigen::VectorXd  x(Eigen::VectorXd::Random(c.cols())), y, y2, x2(Eigen::VectorXd::Random(c.rows()));
+     
      long start, stop;
      for(int i = 0; i<100; i++)
             y.noalias() = c*x;
-
      start = get_time();
-     for(int i = 0; i<100000; i++)
+     for(int i = 0; i<400000; i++)
             y.noalias() = c*x;
      stop = get_time();
+     std::cout << "full: "  <<  ((double)(stop - start))/400000 << std::endl;
 
-     std::cout << "full: "  <<  ((double)(stop - start))/100000 << std::endl;
-
-     for(int i = 0; i<100; i++)
+     for(int i = 0; i<100; i++) {
             y.noalias() = Aii_R->selfadjointView<Eigen::Lower>()*x;
-
+            y2.noalias() = (*Aic_R)*x;
+     }
      start = get_time();
-     for(int i = 0; i<100000; i++)
+     for(int i = 0; i<400000; i++) {
             y.noalias() = Aii_R->selfadjointView<Eigen::Lower>()*x;
+            y2.noalias() = (*Aic_R)*x;
+     }
      stop = get_time();
+     std::cout << "symmetric: "  <<  ((double)(stop - start))/400000 << std::endl;
+     
+     for(int i = 0; i<100; i++)
+            y.noalias() = c.transpose()*x2;
+     start = get_time();
+     for(int i = 0; i<400000; i++)
+            y.noalias() = c.transpose()*x2;
+     stop = get_time();
+     std::cout << "transposed full: "  <<  ((double)(stop - start))/400000 << std::endl;
+     
+     y2 = Eigen::VectorXd::Random(Aic_R->rows());
+     for(int i = 0; i<100; i++) {
+            y.noalias() = Aii_R->selfadjointView<Eigen::Lower>()*x + Aic_R->transpose()*y2;
+            //y += Aic_R->transpose()*y2;
+     }
+     start = get_time();
+     for(int i = 0; i<400000; i++) {
+            y.noalias() = Aii_R->selfadjointView<Eigen::Lower>()*x;
+            //y += Aic_R->transpose()*y2;
+     }
+     stop = get_time();
+     std::cout << "transposed symmetric: "  <<  ((double)(stop - start))/400000 << std::endl;
 
-     std::cout << "Symmetric: "  <<  ((double)(stop - start))/100000 << std::endl;
 
      /*
 

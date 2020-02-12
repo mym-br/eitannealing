a38822109f3461a147e1aebc81abe057d9812841
Thiago Martins <thiago@usp.br>
2020-02-12 14:41:38 -0300

    full solution test

--- tests/test_lb_solution.cpp
+++ tests/test_lb_solution.cpp
@@ -3,9 +3,10 @@
 #include <iostream>
 #include "../src/problem.h"
 #include "../src/nodecoefficients.h"
-#include "../src/solver_lb.h"
-#include "../src/observations_complex.h"
 #include "../src/intcoef.h"
+#include "../src/solution_lb.h"
+#include "../src/gradientnormregularisation.h"
+
 
 #include <sys/time.h>
 #include <sys/resource.h>
@@ -22,53 +23,48 @@ unsigned long get_time()
 int main(int argc, char *argv[])
  {
      bool is2d;
-     if(argc <2) {
-    	 std::cerr << "need mesh filename\n";
+     if(argc <4) {
+    	 std::cerr << "need mesh filename, currents and tensions\n";
     	 return 1;
      }
      std::cout << "Parsing mesh file..." << std::flush;
      std::shared_ptr<problem> input(problem::createNewProblem(argv[1], &is2d));
+     observations<double> *readingsScalar;
      input->initProblem(argv[1]);
      input->buildNodeCoefficients();
+     input->prepareSkeletonMatrix();
+     input->createCoef2KMatrix();
+     gradientNormRegularisation::initInstance(input);
      std::cout << "Done\n" << std::flush;
      std::cout << "Solution with " << input->getNumCoefficients() << " coefficients\n" << std::flush;
+     
+     std::cout << "Parsing observations..." << std::flush;
+     readingsScalar = new observations<double>;
+     readingsScalar->initObs((const char **)&argv[2], argv[3], input->getNodesCount(), input->getGenericElectrodesCount());
+     std::cout << "Done\n" << std::flush;
+     
      std::cout << "Preparing dummy solution..." << std::flush;
      double *sol = new double[input->getNumCoefficients()];
-     for(int i=0;i<input->getNumCoefficients();i++) sol[i]=1.0;
-
-
-     matrix *Aii, *Aic, *Acc;
-     std::cout << "Assembling matrices..." << std::flush;
-     assembleProblemMatrix_lb(sol, &Aii, &Aic, &Acc, *input);
+     for(int i=0;i<input->getNumCoefficients();i++) sol[i]=(mincond+maxcond)/2;
      std::cout << "Done\n" << std::flush;
+     init_genrand64(1);
+     std::unique_ptr<solution_lb> current, next;
+     shuffleData sdata;
+     std::unique_ptr<shuffler> sh;
+     std::cout << "Evaluating initial solution..." << std::flush;     
+     current.reset(new solution_lb(input, *readingsScalar, sol));
+     current->saturate();
+     std::cout << "Done\n" << std::flush;
+     std::cout << "Initial values: " << current->getDMin() << " (min) " << current->getDMax() << " (max)\n";
+     
+     std::cout << "generating next solution..." << std::flush;     
+     sh.reset(new shuffler(input, readingsScalar));
+     next.reset(current->shuffle(&sdata, *sh));
+     std::cout << "Done\n" << std::flush;
+     std::cout << "Next solution Initial values: " << next->getDMin() << " (min) " << next->getDMax() << " (max)\n";
+     
+     
 
-     Eigen::VectorXd V(32), J(32);
-
-     V.fill(0.0);
-     V(4) = -30.738288239574686;
-     V(0) = 30.733620796726793;
-     V(5) = -12.495870755237416;
-     V(31) = 12.49545757830731;
-     V(1) = 8.627724263627385;
-     V(3) = -8.624816166327843;
-
-     J.fill(0.0);
-     J(0) = 1.0;
-     J(4) = -1.0;
-
-
-     LB_Solver::Preconditioner *pre;
-     pre = LB_Solver::makePreconditioner(*Aii, *Aic);
-
-     LB_Solver *solver;
-     solver = new LB_Solver(Aii, Aic, Acc, J, V, *pre, 0.002);
-
-     std::cout << "Initial values: " << solver->getMinErrorl2Estimate() << " (min) " << solver->getMinErrorl2Estimate() << " (max)\n";
-     for(int i=0; i<50;i++) {         
-         solver->do_iteration();
-         std::cout << i << ": " << solver->getMinErrorl2Estimate() << " (min) " << solver->getMinErrorl2Estimate() << " (max)\n";
-     }
-     solver->do_iteration();
 
      return 0;
  }

8eaafaaf473b0a1c65e4389e5935e61fa5eb209b
aksato <andre.kubagawa@usp.br>
2020-07-10 15:57:03 -0300

    Implement generator for multiple forward solutions in python library

--- libs/pyeitsolver/pyeitsolver.py
+++ libs/pyeitsolver/pyeitsolver.py
@@ -1,13 +1,14 @@
 from . import _pyeitsolver
 import pathlib
 
-def init(meshFilename, currentsFilename):
+def forwardSolve(conductivities, meshFilename, currentsFilename):
     if(pathlib.Path(meshFilename).exists() == False):
         raise Exception('Input mesh file not found. The value was: {}'.format(meshFilename))
     if(pathlib.Path(currentsFilename).exists() == False):
         raise Exception('Input currents file not found. The value was: {}'.format(currentsFilename))
 
-    return _pyeitsolver.init(meshFilename, currentsFilename)
-
-def forwardSolve(conds):
-    return _pyeitsolver.solveForwardProblem(conds)
+    problemData = _pyeitsolver.init(meshFilename, currentsFilename)
+    for cond in conductivities:
+        if(len(cond) != problemData['coeffCount']):
+            raise Exception('Incorrect number of conductivities, should be {}. The value was: {}'.format(problemData['coeffCount'], len(cond)))
+        yield _pyeitsolver.solveForwardProblem(cond)

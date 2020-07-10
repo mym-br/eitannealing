a79a1099e2ba3a4d7ad359ee17f894a84131bcf6
aksato <andre.kubagawa@usp.br>
2020-07-10 15:28:53 -0300

    Check if files exists in library wrapper

--- libs/pyeitsolver/pyeitsolver.py
+++ libs/pyeitsolver/pyeitsolver.py
@@ -1,6 +1,12 @@
 from . import _pyeitsolver
+import pathlib
 
 def init(meshFilename, currentsFilename):
+    if(pathlib.Path(meshFilename).exists() == False):
+        raise Exception('Input mesh file not found. The value was: {}'.format(meshFilename))
+    if(pathlib.Path(currentsFilename).exists() == False):
+        raise Exception('Input currents file not found. The value was: {}'.format(currentsFilename))
+
     return _pyeitsolver.init(meshFilename, currentsFilename)
 
 def forwardSolve(conds):

196f13af8570d03c6b817d7647bcb51127012393
aksato <andre.kubagawa@usp.br>
2020-07-12 01:03:30 -0300

    Separate initialization and solver for python library interface

--- libs/pyeitsolver/pyeitsolver.py
+++ libs/pyeitsolver/pyeitsolver.py
@@ -1,14 +1,12 @@
 from . import _pyeitsolver
 import pathlib
 
-def forwardSolve(conductivities, meshFilename, currentsFilename):
+def init(meshFilename, currentsFilename):
     if(pathlib.Path(meshFilename).exists() == False):
         raise Exception('Input mesh file not found. The value was: {}'.format(meshFilename))
     if(pathlib.Path(currentsFilename).exists() == False):
         raise Exception('Input currents file not found. The value was: {}'.format(currentsFilename))
+    return _pyeitsolver.init(meshFilename, currentsFilename)
 
-    problemData = _pyeitsolver.init(meshFilename, currentsFilename)
-    for cond in conductivities:
-        if(len(cond) != problemData['coeffCount']):
-            raise Exception('Incorrect number of conductivities, should be {}. The value was: {}'.format(problemData['coeffCount'], len(cond)))
-        yield _pyeitsolver.solveForwardProblem(cond)
+def forwardSolve(conductivities):
+    return _pyeitsolver.solveForwardProblem(conductivities)

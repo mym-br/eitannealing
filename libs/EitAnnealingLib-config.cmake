f4e9a40ca6be0e16c7094caf78a89a26eb054b4d
GUILHERME DURAN <guiduran@usp.br>
2021-02-11 17:49:08 -0300

    Rename eitsolver lib

--- libs/EitAnnealingLib-config.cmake
+++ libs/EitAnnealingLib-config.cmake
@@ -1,3 +1,3 @@
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include(${SELF_DIR}/lib/eitannealinglib-targets.cmake)
-get_filename_component(EitAnnealing_INCLUDE_DIRS "${SELF_DIR}/include" ABSOLUTE)
+get_filename_component(EitAnnealingLib_INCLUDE_DIRS "${SELF_DIR}/include" ABSOLUTE)

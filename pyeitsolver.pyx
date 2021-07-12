71dbced97db30db1de6e7956c03cab4034d45c17
Andre Sato <andre.kubagawa@usp.br>
2021-07-12 11:52:45 -0300

    Convert map keys from bytes to Python string

--- pyeitsolver.pyx
+++ pyeitsolver.pyx
@@ -2,13 +2,24 @@
  
 cimport cpyeitsolver
 import pathlib
+from libcpp.string cimport string
+from libcpp.map cimport map
+from cython.operator cimport dereference, postincrement
 
 cpdef init(meshFilename, currentsFilename):
     if(pathlib.Path(meshFilename).exists() == False):
         raise Exception('Input mesh file not found. The value was: {}'.format(meshFilename))
     if(pathlib.Path(currentsFilename).exists() == False):
         raise Exception('Input currents file not found. The value was: {}'.format(currentsFilename))
-    return cpyeitsolver.init(meshFilename.encode(), currentsFilename.encode())
+    modelInfo = cpyeitsolver.init(meshFilename.encode(), currentsFilename.encode())
+    
+    # Convert map to dict with string keys
+    modelInfoStr = {}
+    cdef map[string,int].iterator it = modelInfo.begin()
+    while(it != modelInfo.end()):
+        modelInfoStr[dereference(it).first.decode()] = dereference(it).second
+        postincrement(it)
+    return modelInfoStr
 
 cpdef forwardSolve(conductivities):
     return cpyeitsolver.solveForwardProblem(conductivities)

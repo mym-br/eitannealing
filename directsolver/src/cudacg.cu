5bc4f559cf691abf392afb990803ae940ad25faf
Andre Sato <andresato@hotmail.com>
2022-12-13 21:41:13 -0300

    Update directsolver to CUDA 11 for CUBLAS execution

--- directsolver/src/cudacg.cu
+++ directsolver/src/cudacg.cu
@@ -27,7 +27,7 @@ std::tuple<long, long, int> runCusparseCublasCG(std::vector<int> &I, std::vector
 	float *d_zm1, *d_zm2, *d_rm2;
 	float *d_r, *d_p, *d_omega, *d_y;
 	float *d_valsILU0;
-	float *valsILU0;
+	void *buffer = NULL;
 	float dot, numerator, denominator, nalpha;
 	const float floatone = 1.0;
 	const float floatzero = 0.0;
@@ -40,22 +40,14 @@ std::tuple<long, long, int> runCusparseCublasCG(std::vector<int> &I, std::vector
 	cublasStatus_t cublasStatus;
 	cublasStatus = cublasCreate(&cublasHandle);
 
-	cublasStatus;
-
 	/* Create CUSPARSE context */
 	cusparseHandle_t cusparseHandle = 0;
 	cusparseStatus_t cusparseStatus;
 	cusparseStatus = cusparseCreate(&cusparseHandle);
 
-	cusparseStatus;
-
 	/* Description of the A matrix*/
 	cusparseMatDescr_t descr = 0;
 	cusparseStatus = cusparseCreateMatDescr(&descr);
-
-	cusparseStatus;
-
-	/* Define the properties of the matrix */
 	cusparseSetMatType(descr, CUSPARSE_MATRIX_TYPE_GENERAL);
 	cusparseSetMatIndexBase(descr, CUSPARSE_INDEX_BASE_ZERO);
 
@@ -68,60 +60,111 @@ std::tuple<long, long, int> runCusparseCublasCG(std::vector<int> &I, std::vector
 	cudaMalloc((void **)&d_r, N * sizeof(float));
 	cudaMalloc((void **)&d_p, N * sizeof(float));
 	cudaMalloc((void **)&d_omega, N * sizeof(float));
+	cudaMalloc((void **)&d_valsILU0, nz * sizeof(float));
+	cudaMalloc((void **)&d_zm1, (N) * sizeof(float));
+	cudaMalloc((void **)&d_zm2, (N) * sizeof(float));
+	cudaMalloc((void **)&d_rm2, (N) * sizeof(float));
 
+	/* Wrap raw data into cuSPARSE generic API objects */
+	cusparseSpMatDescr_t matA = NULL;
+	cusparseStatus = cusparseCreateCsr(&matA, N, N, nz, d_row, d_col, d_val,
+									CUSPARSE_INDEX_32I, CUSPARSE_INDEX_32I,
+									CUSPARSE_INDEX_BASE_ZERO, CUDA_R_32F);
+	cusparseDnVecDescr_t vecp = NULL;
+	cusparseStatus = cusparseCreateDnVec(&vecp, N, d_p, CUDA_R_32F);
+	cusparseDnVecDescr_t vecomega = NULL;
+	cusparseStatus = cusparseCreateDnVec(&vecomega, N, d_omega, CUDA_R_32F);
+
+	/* Initialize problem data */
 	cudaMemcpy(d_col, J.data(), nz * sizeof(int), cudaMemcpyHostToDevice);
 	cudaMemcpy(d_row, I.data(), (N + 1) * sizeof(int), cudaMemcpyHostToDevice);
 	cudaMemcpy(d_val, val.data(), nz * sizeof(float), cudaMemcpyHostToDevice);
 	cudaMemcpy(d_x, x.data(), N * sizeof(float), cudaMemcpyHostToDevice);
 	cudaMemcpy(d_r, rhs.data(), N * sizeof(float), cudaMemcpyHostToDevice);
 
+	/* Create ILU(0) info object */
+	csrilu02Info_t infoILU = NULL;
+	cusparseStatus = cusparseCreateCsrilu02Info(&infoILU);
+
+	/* Create L factor descriptor and triangular solve info */
+	cusparseMatDescr_t descrL = NULL;
+	cusparseStatus = cusparseCreateMatDescr(&descrL);
+	cusparseStatus = cusparseSetMatType(descrL, CUSPARSE_MATRIX_TYPE_GENERAL);
+	cusparseStatus = cusparseSetMatIndexBase(descrL, CUSPARSE_INDEX_BASE_ZERO);
+	cusparseStatus = cusparseSetMatFillMode(descrL, CUSPARSE_FILL_MODE_LOWER);
+	cusparseStatus = cusparseSetMatDiagType(descrL, CUSPARSE_DIAG_TYPE_UNIT);
+	csrsv2Info_t infoL = NULL;
+	cusparseStatus = cusparseCreateCsrsv2Info(&infoL);
+  
+	/* Create U factor descriptor and triangular solve info */
+	cusparseMatDescr_t descrU = NULL;
+	cusparseStatus = cusparseCreateMatDescr(&descrU);
+	cusparseStatus = cusparseSetMatType(descrU, CUSPARSE_MATRIX_TYPE_GENERAL);
+	cusparseStatus = cusparseSetMatIndexBase(descrU, CUSPARSE_INDEX_BASE_ZERO);
+	cusparseStatus = cusparseSetMatFillMode(descrU, CUSPARSE_FILL_MODE_UPPER);
+	cusparseStatus = cusparseSetMatDiagType(descrU, CUSPARSE_DIAG_TYPE_NON_UNIT);
+	csrsv2Info_t infoU = NULL;
+	cusparseStatus = cusparseCreateCsrsv2Info(&infoU);
+  
+	/* Allocate workspace for cuSPARSE */
+	size_t bufferSize = 0;
+	size_t tmp = 0;
+	int stmp = 0;
+	cusparseStatus = cusparseSpMV_bufferSize(
+		cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, &floatone, matA, vecp,
+		&floatzero, vecomega, CUDA_R_32F, CUSPARSE_SPMV_ALG_DEFAULT, &tmp);
+	if (tmp > bufferSize) {
+	  bufferSize = stmp;
+	}
+	cusparseStatus = cusparseScsrilu02_bufferSize(
+		cusparseHandle, N, nz, descr, d_val, d_row, d_col, infoILU, &stmp);
+	if (stmp > bufferSize) {
+	  bufferSize = stmp;
+	}
+	cusparseStatus = cusparseScsrsv2_bufferSize(
+		cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, N, nz, descrL, d_val,
+		d_row, d_col, infoL, &stmp);
+	if (stmp > bufferSize) {
+	  bufferSize = stmp;
+	}
+	cusparseStatus = cusparseScsrsv2_bufferSize(
+		cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, N, nz, descrU, d_val,
+		d_row, d_col, infoU, &stmp);
+	if (stmp > bufferSize) {
+	  bufferSize = stmp;
+	}
+	cudaMalloc(&buffer, bufferSize);
+
 	/* Preconditioned Conjugate Gradient using ILU.
 	--------------------------------------------
 	Follows the description by Golub & Van Loan, "Matrix Computations 3rd ed.", Algorithm 10.3.1  */
 
-	int nzILU0 = 2 * N - 1;
-	valsILU0 = (float *)malloc(nz * sizeof(float));
+	/* Perform analysis for ILU(0) */
+	cusparseStatus = cusparseScsrilu02_analysis(
+		cusparseHandle, N, nz, descr, d_val, d_row, d_col, infoILU,
+		CUSPARSE_SOLVE_POLICY_USE_LEVEL, buffer);
+  
+	/* Copy A data to ILU(0) vals as input*/
+	cudaMemcpy(d_valsILU0, d_val, nz * sizeof(float),
+							   cudaMemcpyDeviceToDevice);
+  
+	/* generate the ILU(0) factors */
+	cusparseStatus = cusparseScsrilu02(cusparseHandle, N, nz, descr, d_valsILU0,
+									  d_row, d_col, infoILU,
+									  CUSPARSE_SOLVE_POLICY_USE_LEVEL, buffer);
 
-	cudaMalloc((void **)&d_valsILU0, nz * sizeof(float));
-	cudaMalloc((void **)&d_zm1, (N) * sizeof(float));
-	cudaMalloc((void **)&d_zm2, (N) * sizeof(float));
-	cudaMalloc((void **)&d_rm2, (N) * sizeof(float));
-
-	/* create the analysis info object for the A matrix */
-	cusparseSolveAnalysisInfo_t infoA = 0;
-	cusparseStatus = cusparseCreateSolveAnalysisInfo(&infoA);
-
-	/* Perform the analysis for the Non-Transpose case */
-	cusparseStatus = cusparseScsrsv_analysis(cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE,
-		N, nz, descr, d_val, d_row, d_col, infoA);
-
-	/* Copy A data to ILU0 vals as input*/
-	cudaMemcpy(d_valsILU0, d_val, nz * sizeof(float), cudaMemcpyDeviceToDevice);
-
-	/* generate the Incomplete LU factor H for the matrix A using cudsparseScsrilu0 */
-	cusparseStatus = cusparseScsrilu0(cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, N, descr, d_valsILU0, d_row, d_col, infoA);
 	auto t2 = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<double> time_analyser = t2 - t1;
 	std::cout << "Cublas analyser on A used " << std::chrono::duration_cast<std::chrono::microseconds>(time_analyser).count() << " us." << std::endl;
+  
+	/* perform triangular solve analysis */
+	cusparseStatus = cusparseScsrsv2_analysis(cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE,
+								N, nz, descrL, d_valsILU0, d_row, d_col, infoL,
+								CUSPARSE_SOLVE_POLICY_USE_LEVEL, buffer);
 
-	/* Create info objects for the ILU0 preconditioner */
-	cusparseSolveAnalysisInfo_t info_u;
-	cusparseCreateSolveAnalysisInfo(&info_u);
-
-	cusparseMatDescr_t descrL = 0;
-	cusparseStatus = cusparseCreateMatDescr(&descrL);
-	cusparseSetMatType(descrL, CUSPARSE_MATRIX_TYPE_GENERAL);
-	cusparseSetMatIndexBase(descrL, CUSPARSE_INDEX_BASE_ZERO);
-	cusparseSetMatFillMode(descrL, CUSPARSE_FILL_MODE_LOWER);
-	cusparseSetMatDiagType(descrL, CUSPARSE_DIAG_TYPE_UNIT);
-
-	cusparseMatDescr_t descrU = 0;
-	cusparseStatus = cusparseCreateMatDescr(&descrU);
-	cusparseSetMatType(descrU, CUSPARSE_MATRIX_TYPE_GENERAL);
-	cusparseSetMatIndexBase(descrU, CUSPARSE_INDEX_BASE_ZERO);
-	cusparseSetMatFillMode(descrU, CUSPARSE_FILL_MODE_UPPER);
-	cusparseSetMatDiagType(descrU, CUSPARSE_DIAG_TYPE_NON_UNIT);
-	cusparseStatus = cusparseScsrsv_analysis(cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, N, nz, descrU, d_val, d_row, d_col, info_u);
+	cusparseStatus = cusparseScsrsv2_analysis(cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE,
+								N, nz, descrU, d_valsILU0, d_row, d_col, infoU,
+								CUSPARSE_SOLVE_POLICY_USE_LEVEL, buffer);
 
 	/* reset the initial guess of the solution to zero */
 	for (int i = 0; i < N; i++) x[i] = 0.0;
@@ -149,13 +192,15 @@ std::tuple<long, long, int> runCusparseCublasCG(std::vector<int> &I, std::vector
 		cudaEventRecord(startTotal);
 		cudaEventRecord(startTri);
 #endif // CGTIMING
-		// Forward Solve, we can re-use infoA since the sparsity pattern of A matches that of L
-		cusparseStatus = cusparseScsrsv_solve(cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, N, &floatone, descrL,
-			d_valsILU0, d_row, d_col, infoA, d_r, d_y);
-
-		// Back Substitution
-		cusparseStatus = cusparseScsrsv_solve(cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, N, &floatone, descrU,
-			d_valsILU0, d_row, d_col, info_u, d_y, d_zm1);
+		// preconditioner application: d_zm1 = U^-1 L^-1 d_r
+		cusparseStatus = cusparseScsrsv2_solve(
+			cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, N, nz, &floatone,
+			descrL, d_valsILU0, d_row, d_col, infoL, d_r, d_y,
+			CUSPARSE_SOLVE_POLICY_USE_LEVEL, buffer);
+		cusparseStatus = cusparseScsrsv2_solve(
+			cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, N, nz, &floatone,
+			descrU, d_valsILU0, d_row, d_col, infoU, d_y, d_zm1,
+			CUSPARSE_SOLVE_POLICY_USE_LEVEL, buffer);
 #ifdef CGTIMING
 		cudaEventRecord(stopTri);
 #endif // CGTIMING
@@ -177,7 +222,9 @@ std::tuple<long, long, int> runCusparseCublasCG(std::vector<int> &I, std::vector
 #ifdef CGTIMING
 		cudaEventRecord(startSpmv);
 #endif // CGTIMING
-		cusparseScsrmv(cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, N, N, nzILU0, &floatone, descrU, d_val, d_row, d_col, d_p, &floatzero, d_omega);
+		cusparseSpMV(
+			cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, &floatone, matA, vecp,
+			&floatzero, vecomega, CUDA_R_32F, CUSPARSE_SPMV_ALG_DEFAULT, buffer);
 #ifdef CGTIMING
 		cudaEventRecord(stopSpmv);
 #endif // CGTIMING
@@ -216,16 +263,22 @@ std::tuple<long, long, int> runCusparseCublasCG(std::vector<int> &I, std::vector
 	std::cout << "Average cublas/cusparse iteration time breakdown: " << totalTriangularTime << " (triangular solver) " << totalSpmvTime << " (spmv) " << totalItTime - totalTriangularTime - totalSpmvTime << " (remaining) " << totalItTime << " (total)." << std::endl;
 #endif // CGTIMING
 
-	/* Destroy parameters */
-	cusparseDestroySolveAnalysisInfo(infoA);
-	cusparseDestroySolveAnalysisInfo(info_u);
+	/* Destroy descriptors */
+	cusparseDestroyCsrsv2Info(infoU);
+	cusparseDestroyCsrsv2Info(infoL);
+	cusparseDestroyCsrilu02Info(infoILU);
+	cusparseDestroyMatDescr(descrL);
+	cusparseDestroyMatDescr(descrU);
+	cusparseDestroyMatDescr(descr);
+	cusparseDestroySpMat(matA);
+	cusparseDestroyDnVec(vecp);
+	cusparseDestroyDnVec(vecomega);
 
 	/* Destroy contexts */
 	cusparseDestroy(cusparseHandle);
 	cublasDestroy(cublasHandle);
 
 	/* Free device memory */
-	free(valsILU0);
 	cudaFree(d_col);
 	cudaFree(d_row);
 	cudaFree(d_val);

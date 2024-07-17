f7a98bfd26967a8103773b3f2c0469ded037b8de
Andre Sato <andre.kubagawa@usp.br>
2024-07-16 21:12:37 -0300

    implement petsc solver inside C++ code for pybind11 library

--- python/src/complex.cpp
+++ python/src/complex.cpp
@@ -3,6 +3,9 @@
 #include <pybind11/stl.h>
 #include <filesystem>
 #include <Eigen/Core>
+#ifdef USE_PETSC
+#include <petscksp.h>
+#endif
 #include <memory>
 #include <tuple>
 #include <map>
@@ -13,6 +16,12 @@
 #include "observations.h"
 
 #define STRINGIFY(x) #x
+#define CHKERRTHROW(ierr)                                     \
+    if (PetscUnlikely(ierr))                                  \
+    {                                                         \
+        throw std::runtime_error(PetscErrorMessageStr(ierr)); \
+    }
+
 #define CURRENT_FREQUENCY 275000
 #define PARASITE_CAPACITANCY 80E-12
 
@@ -20,12 +29,35 @@ namespace fs = std::filesystem;
 
 namespace pyeitsolver
 {
+#ifdef USE_PETSC
+    std::string PetscErrorMessageStr(PetscErrorCode ierr)
+    {
+        const char *msg;
+        PetscErrorMessage(ierr, &msg, PETSC_NULLPTR);
+        return std::string(msg);
+    }
+#endif
+
     class EitComplexSolver
     {
     public:
         EitComplexSolver(std::string meshFilename, std::string currentFilename)
         {
             init(meshFilename, currentFilename);
+#ifdef USE_PETSC
+            PetscErrorCode ierr = PetscInitialize(PETSC_NULLPTR, PETSC_NULLPTR, PETSC_NULLPTR, PETSC_NULLPTR);
+            if (ierr)
+            {
+                throw std::runtime_error("Could not initialize PETSc");
+            }
+#endif
+        }
+
+        ~EitComplexSolver()
+        {
+#ifdef USE_PETSC
+            PetscErrorCode ierr = PetscFinalize();
+#endif
         }
 
         std::map<std::string, int> getProblemInfo()
@@ -38,7 +70,8 @@ namespace pyeitsolver
                 {"first_electrode_idx", input->getGroundNode() - input->getGenericElectrodesCount() + 1}};
         }
 
-        std::tuple<Eigen::VectorXi, Eigen::VectorXi, Eigen::VectorXcd> getStiffnessCOO(Eigen::VectorXd &conductivities) {
+        std::tuple<Eigen::VectorXi, Eigen::VectorXi, Eigen::VectorXcd> getStiffnessCOO(Eigen::VectorXd &conductivities)
+        {
             // Check conductivity vector size
             auto n = conductivities.size();
             if (n != input->getNumCoefficients())
@@ -61,7 +94,8 @@ namespace pyeitsolver
 
             int i = 0;
             for (int k = 0; k < m1->outerSize(); ++k)
-                for (matrixcomplex::InnerIterator it(*m1, k); it; ++it) {
+                for (matrixcomplex::InnerIterator it(*m1, k); it; ++it)
+                {
                     rows[i] = it.row();
                     cols[i] = it.col();
                     values[i] = it.value();
@@ -71,17 +105,148 @@ namespace pyeitsolver
             return std::tuple<Eigen::VectorXi, Eigen::VectorXi, Eigen::VectorXcd>(rows, cols, values);
         }
 
-        Eigen::MatrixXd getCurrentsVectors() {
+        Eigen::MatrixXd getCurrentsVectors()
+        {
             int numCols = input->getCurrentVector(0, readings.get()).size();
             int numRows = readings->getCurrentsCount();
             Eigen::MatrixXd currentsMatrix(numRows, numCols);
 
-
-            for (int pattern = 0; pattern < numRows; ++pattern) {
+            for (int pattern = 0; pattern < numRows; ++pattern)
+            {
                 currentsMatrix.row(pattern) = input->getCurrentVector(pattern, readings.get());
             }
             return currentsMatrix;
         }
+
+#ifdef USE_PETSC
+        Eigen::MatrixXcd solveForwardProblem(Eigen::VectorXcd &conductivities, bool meshPotentials = false)
+        {
+            Vec x, b; /* solution, right-hand-side */
+            Mat A;    /* linear system matrix */
+            KSP ksp;  /* linear solver context */
+            PetscErrorCode ierr;
+
+            // Check conductivity vector size
+            auto n = conductivities.size();
+            if (n != input->getNumCoefficients())
+                throw std::runtime_error("Wrong conductivities vector size " + std::to_string(n) + " (should be " + std::to_string(input->getNumCoefficients()) + ")");
+
+            // Map node conductivities to coefficient indices
+            Eigen::VectorXcd v = Eigen::VectorXcd::Zero(n);
+            Eigen::VectorXi indices = createIndicesVector(input);
+            v(indices) = conductivities;
+
+            // Create FEM conductivity matrix
+            matrixcomplex *m1;
+            input->assembleProblemMatrix(&v[0], &m1);
+            input->addMatrixCapacitances(&m1);
+
+            // Create vectors
+            ierr = VecCreate(PETSC_COMM_WORLD, &x);
+            CHKERRTHROW(ierr);
+            ierr = VecSetSizes(x, PETSC_DECIDE, n);
+            CHKERRTHROW(ierr);
+            ierr = VecSetFromOptions(x);
+            CHKERRTHROW(ierr);
+            ierr = VecDuplicate(x, &b);
+            CHKERRTHROW(ierr);
+
+            // Create matrix
+            ierr = MatCreate(PETSC_COMM_WORLD, &A);
+            CHKERRTHROW(ierr);
+            ierr = MatSetSizes(A, PETSC_DECIDE, PETSC_DECIDE, n, n);
+            CHKERRTHROW(ierr);
+            ierr = MatSetFromOptions(A);
+            CHKERRTHROW(ierr);
+            ierr = MatSetUp(A);
+            CHKERRTHROW(ierr);
+
+            // Assemble matrix A from Eigen::SparseMatrix
+            for (int k = 0; k < m1->outerSize(); ++k)
+            {
+                for (matrixcomplex::InnerIterator it(*m1, k); it; ++it)
+                {
+                    PetscInt row = it.row();
+                    PetscInt col = it.col();
+                    PetscScalar value = it.value();
+                    ierr = MatSetValues(A, 1, &row, 1, &col, &value, INSERT_VALUES);
+                    CHKERRTHROW(ierr);
+                    if (row == col)
+                        continue;
+                    ierr = MatSetValues(A, 1, &col, 1, &row, &value, INSERT_VALUES);
+                    CHKERRTHROW(ierr);
+                }
+            }
+
+            // Assemble the matrix
+            ierr = MatAssemblyBegin(A, MAT_FINAL_ASSEMBLY);
+            CHKERRTHROW(ierr);
+            ierr = MatAssemblyEnd(A, MAT_FINAL_ASSEMBLY);
+            CHKERRTHROW(ierr);
+
+            Eigen::MatrixXcd potentials(electrodeCount, n);
+            for (int pattern = 0; pattern < readings->getCurrentsCount(); pattern++)
+            {
+                // Set right-hand side vector from Eigen::VectorXcd
+                Eigen::VectorXd b_eigen = input->getCurrentVector(pattern, readings.get());
+                for (PetscInt i = 0; i < n; ++i)
+                {
+                    std::complex<double> value = b_eigen[i];
+                    ierr = VecSetValue(b, i, value, INSERT_VALUES);
+                    CHKERRTHROW(ierr);
+                }
+
+                ierr = VecAssemblyBegin(b);
+                CHKERRTHROW(ierr);
+                ierr = VecAssemblyEnd(b);
+                CHKERRTHROW(ierr);
+
+                // Create linear solver context
+                ierr = KSPCreate(PETSC_COMM_WORLD, &ksp);
+                CHKERRTHROW(ierr);
+                ierr = KSPSetOperators(ksp, A, A);
+                CHKERRTHROW(ierr);
+
+                // Set linear solver options
+                ierr = KSPSetFromOptions(ksp);
+                CHKERRTHROW(ierr);
+
+                // Solve linear system
+                ierr = KSPSolve(ksp, b, x);
+                CHKERRTHROW(ierr);
+
+                // // View the solution
+                // ierr = VecView(x, PETSC_VIEWER_STDOUT_WORLD);
+                // CHKERRTHROW(ierr);
+
+                // Convert PETSc vector x to Eigen::VectorXcd
+                PetscInt size;
+                ierr = VecGetSize(x, &size);
+                CHKERRTHROW(ierr);
+
+                const PetscScalar *x_array;
+                ierr = VecGetArrayRead(x, &x_array);
+                CHKERRTHROW(ierr);
+
+                for (PetscInt i = 0; i < size; ++i)
+                {
+                    potentials.row(pattern)[i] = x_array[i];
+                }
+            }
+
+            // Free work space
+            ierr = VecDestroy(&x);
+            CHKERRTHROW(ierr);
+            ierr = VecDestroy(&b);
+            CHKERRTHROW(ierr);
+            ierr = MatDestroy(&A);
+            CHKERRTHROW(ierr);
+            ierr = KSPDestroy(&ksp);
+            CHKERRTHROW(ierr);
+
+            return potentials;
+        }
+#endif
 
         int nodeCount;
         int electrodeCount;
@@ -109,7 +274,7 @@ namespace pyeitsolver
             input = problem::createNewProblem(meshFilename.c_str(), &is2dProblem);
             input->setIgnoreouterring(true);
             input->setCapacitance(PARASITE_CAPACITANCY);
-		    input->setCurrentFreq(CURRENT_FREQUENCY);
+            input->setCurrentFreq(CURRENT_FREQUENCY);
             input->initProblem(meshFilename.c_str());
 
             // Create FEM sparse matrix and auxiliary structures
@@ -138,10 +303,10 @@ namespace pyeitsolver
             std::iota(temp.begin(), temp.end(), 0); // Fill with sequential integers starting from 0
 
             std::transform(temp.begin(), temp.end(), indices.data(), [&input](int i)
-                        { return input->getNode2Coefficient(i); });
+                           { return input->getNode2Coefficient(i); });
 
             return indices;
-        }        
+        }
     };
 }
 
@@ -161,6 +326,9 @@ PYBIND11_MODULE(_complex, m)
         .def_readwrite("current", &pyeitsolver::EitComplexSolver::current)
         .def("getCOO_formatted_stiffness", &pyeitsolver::EitComplexSolver::getStiffnessCOO)
         .def("get_currents_vectors", &pyeitsolver::EitComplexSolver::getCurrentsVectors)
+#ifdef USE_PETSC
+        .def("solve_forward_problem", &pyeitsolver::EitComplexSolver::solveForwardProblem, "Solves forward problem, returning potentials, for a given conductivity distribution", py::arg("conds"), py::kw_only(), py::arg("mesh_potentials") = false)
+#endif
         .def("__repr__",
              [](const pyeitsolver::EitComplexSolver &a)
              {
